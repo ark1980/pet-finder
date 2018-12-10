0e3186ec7974e8e08732325f0f10af09ee876669
Ali <ar.k@outlook.com>
2018-12-09 16:18:28 -0800

    add Semantic-UI package

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Segment, Button } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Segment
+        inverted
+        textAlign='center'
+        style={{ minHeight: 700, padding: '1em 0em' }}
+        vertical
+      >
+      </Segment>
     );
   }
 }
